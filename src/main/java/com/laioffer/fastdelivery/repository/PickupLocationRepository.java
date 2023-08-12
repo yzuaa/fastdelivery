e24fdc9d0048f033a4692bc0993339458854f207
YuhaoChen830 <36681044+YuhaoChen830@users.noreply.github.com>
2023-08-11 20:38:38 -0500

    Delete PickupLocationRepository.java

--- src/main/java/com/laioffer/fastdelivery/repository/PickupLocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laioffer.fastdelivery.repository;
-
-import com.laioffer.fastdelivery.model.Location;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PickupLocationRepository extends ElasticsearchRepository<Location, Long> {
-}
